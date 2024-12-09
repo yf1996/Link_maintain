f98a1e3829dee4f6a809cb1f5dc8ff7099de1612
yf1996 <997125133@qq.com>
2024-12-09 09:57:28 +0800

    unlimit commrange

--- simulations/chartplot.py
+++ simulations/chartplot.py
@@ -1,30 +1,95 @@
-import pandas as pd
-from omnetpp.scave import results, chart, utils
 import math
-import json
 import numpy as np
+import pandas as pd
+import os
+import subprocess
 import matplotlib.pyplot as plt
 
-folderPath = "./simulations/results/"
-filePath = "1.csv"
 
-# with open(folderPath + filePath, "r", encoding="utf-8") as file:
-#     data = json.load(file)  # 将 JSON 文件内容解析为 Python 对象（通常是字典或列表）
-#     print(data['LinkMaintain-0-20241128-14:13:30-20020']['vectors'][1].keys())
+# collect parameters for query
+"""
+type =~ vector 
+AND runattr:experiment =~ LinkMaintain 
+AND runattr:measurement =~ "$numSectors=*, $rangeMultipleMax=*" 
+AND runattr:replication =~ "*" 
+AND module =~ "Network.radioMedium" 
+AND name =~ CurrentLinkNum:vector
+"""
 
-df = pd.read_csv(folderPath + filePath)
-print(df.iloc[:, 1::2])
-df_data = df.iloc[:, 1::2]
-df_data_1 = df_data.iloc[:, :10].values
-print(df_data_1)
-df_data_2 = df_data.iloc[:, 10:].values
-print(df_data_2)
 
-data_sector_6 = np.mean(df_data_1, axis=1)
-data_sector_8 = np.mean(df_data_2, axis=1)
+def exportToCsv(measurement_name, numSectors, rangeMultipleMax):
+    # get path
+    retval = os.getcwd()
+    print("当前工作目录为 %s" % retval)
+    os.chdir(retval + "./simulations/results")
+    print("当前工作目录为 %s" % os.getcwd())
 
-plt.plot(df.iloc[:, 0].values, data_sector_6, label="6")
-plt.plot(df.iloc[:, 0].values, data_sector_8, label="8")
-plt.legend()
-plt.xlim([0, 1000])
-plt.show()
+    # export data to csv
+    # numSectors = 12
+    # rangeMultipleMax = 1
+    opp_scavetool_exe = "E:/Workplace/omnetpp/omnetpp-6.0/bin/opp_scavetool.exe export "
+    type_filter = "vector"
+    # measurement_name = "CurrentLinkNum"
+    name_filter = f'"{measurement_name}:vector"'
+    runattr_mesurement = (
+        f'\\"$numSectors={numSectors}, $rangeMultipleMax={rangeMultipleMax}\\"'
+    )
+    filter_cmd = (
+        "type =~ "
+        + type_filter
+        + " AND name =~ "
+        + name_filter
+        + " AND runattr:measurement =~ "
+        + runattr_mesurement
+    )
+    outputPath = retval + "/simulations/results_csv/"
+    outputFileName = f"{measurement_name},numSectors-{numSectors},rangeMultipleMax-{rangeMultipleMax}.csv"
+    run_cmd = (
+        opp_scavetool_exe
+        + "-f"
+        + " '"
+        + filter_cmd
+        + "' "
+        + "-o "
+        + outputPath
+        + outputFileName
+        + " -F CSV-S *.vec"
+    )
+    # print(run_cmd)
+    subprocess.run(
+        [f"powershell.exe", "-Command", run_cmd],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True,
+    )
+
+    os.chdir(retval)
+    return outputPath + outputFileName
+
+
+if __name__ == "__main__":
+    numSectors = [8, 10, 12]  # 8 10 12
+    # numSectors = [10]  # 8 10 12
+    # rangeMultipleMax diff
+    rangeMultipleMaxs = [1, 4, 7, 10]
+    rangeMultipleMaxs = [1]
+    measurement_name = "CurrentLinkNum"
+
+    ax = plt.subplot()
+    # export data
+    for rangeMultipleMax in rangeMultipleMaxs:
+        for numSector in numSectors:
+            filename = exportToCsv(
+                measurement_name=measurement_name,
+                numSectors=numSector,
+                rangeMultipleMax=rangeMultipleMax,
+            )
+            print(filename)
+            # read data and plot
+            df = pd.read_csv(filename)
+            timeAxis = df.iloc[:, 0].values
+            data_rep = df.iloc[:, 1:-1:2].values
+            data_avg = np.mean(data_rep, axis=1)
+            ax.plot(timeAxis, data_avg, label=f"{filename}")
+            ax.legend()
+    plt.show()
